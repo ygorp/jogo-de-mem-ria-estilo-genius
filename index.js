a3e0679c3c08a62809a440012355651044bffa06
Ygor Pacheco Nogueira <ygor-nogueira@hotmail.com>
2022-03-22 15:45:24 -0300

    função acender as cores e o tempo, e depois remover a classe

--- index.js
+++ index.js
@@ -23,5 +23,16 @@ let shuffleOrder = () => {
 
   for(let i in order){
     let elementColor = createColorElement(order[i]);
+    lightColor(elementColor, Number(i) + 1);
   }
+}
+
+let lightColor = (element, number) => {
+  number = number * 500;
+  setTimeout(() => {
+    element.classList.add('selected');
+  }, number - 250);
+  setTimeout(() => {
+    element.classList.remover('selected');
+  });
 }
